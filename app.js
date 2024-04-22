8b038a0c0da0542657a3e5a1d7f5ac953b2b0188
expresslifeuk <expresslife555@gmail.com>
2024-04-23 02:28:22 +0600

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-	res.sendFile('index.html');
+	res.sendFile('index.html', { root: './' });
 });
 
 app.get('/users', (req, res) => {
